7cdd38afda798aa8f48aaaa17232d6f2b180946d
Julian Ramirez <julramirezcordero@gmail.com>
2025-10-08 17:52:46 -0600

    Loaded models

---
+++ src/utils/models.py
@@ -0,0 +1,32 @@
+from transformers import GPTNeoXForCausalLM, AutoTokenizer
+
+class PythiaModel:
+    
+    def __init__(self, model_name: str, revision: str, cache_dir: str):
+
+        self.model = GPTNeoXForCausalLM.from_pretrained(
+            model_name,
+            revision=revision,
+            cache_dir=cache_dir
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name,
+            revision=revision,
+            cache_dir=cache_dir
+        )
+
+    def generate(self, prompt:str):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        tokens = self.model.generate(**inputs)
+        return self.tokenizer.decode(tokens[0])
+
+if __name__ == "__main__":
+    model = PythiaModel(
+        model_name="EleutherAI/pythia-70m-deduped",
+        revision="step3000",
+        cache_dir="./pythia-70m-deduped/step3000"
+    )
+    
+    output = model.generate("Hello, I am")
+    print(output)
