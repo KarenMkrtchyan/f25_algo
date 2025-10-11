e4590e80fc4a7361abde99fe0d35ac90e9cd3022
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-10-10 18:36:13 -0700

    small number

--- src/eval.py
+++ src/eval.py
@@ -2,13 +2,12 @@ import torch
 from utils.models import *
 from utils.data.dataset_utils import *
 from utils.data.data_generator import *
-
+from utils.models import PythiaModel
 
 if torch.cuda.is_available():
     print("cuda is available")
 else:
     print("cuda is not available")
-
 
 torch.cuda.empty_cache()
 
@@ -17,13 +16,11 @@ model = PythiaModel(
     revision="step3000",
     cache_dir="./pythia-70m-deduped/step3000"
 )
-
-
-gen = DataGenerator(seed=42)
-data1 = gen.synthetic_data_one(1000)
+# try to load data instead of gen 
+gen = DataGenerator(seed=42) 
+data1 = gen.synthetic_data_one(10)
 
 results = []
-
 
 for i, sample in enumerate(data1):
     prompt = sample["text"]
