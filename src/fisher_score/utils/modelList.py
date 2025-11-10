b56da9f94adf59757f06b11ce9328bc08c61a40c
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-11-09 22:11:39 -0800

    added end layer

--- src/fisher_score/utils/modelList.py
+++ src/fisher_score/utils/modelList.py
@@ -3,13 +3,16 @@ modelslist = [
     {
         'name': "meta-llama/Meta-Llama-3-8B",
         'start_layer': 15,
+        'end_layer': 20
     },
     {
         'name': "Qwen/Qwen2.5-7B",
         'start_layer': -1,
+        'end_layer': 20
     },
     {
         'name': "google/gemma-7b",
         'start_layer': -1,
+        'end_layer': 20
     }
 ]
