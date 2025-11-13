d160d68c2ed10f267c23d988ed8c325067657187
julianDesktop <julianramirezcordero@gmail.com>
2025-11-13 03:57:27 +0000

    added accuracy visualizations

--- src/Experiments/accuracy_digit_drop.py
+++ src/Experiments/accuracy_digit_drop.py
@@ -4,11 +4,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from test_suite.eval import run_benchmark
 
-model = "Qwen/Qwen2.5-7B"
+model_names = [
+    "Qwen/Qwen2.5-7B", 
+    "Qwen/Qwen2.5-3B", 
+    "google/gemma-2-2b", 
+    "meta-llama/Llama-3.2-3B",
+    ]
 
 tasks=[
        "greater_than_3_digit", 
@@ -29,17 +33,40 @@ tasks=[
        "greater_than_18_digit",
        ]
 
-for task in tasks:
+accuracy_df = pd.DataFrame(columns=['model_name', 'accuracy', 'digits'])
 
-    df = run_benchmark(
-        models=[model],
-        task_name=task,
-        num_fewshot=5,
-        limit=1000,
-        output_dir= "../test_suite/dataruns/benchmarks/by_digit",
-        run=1
-    )
+for model in model_names:
 
+    digit_counter=3
+    for task in tasks:
 
+        df = run_benchmark(
+            models=[model],
+            task_name=task,
+            num_fewshot=5,
+            limit=1000,
+            output_dir= "../test_suite/dataruns/benchmarks/by_digit",
+            run=1
+        )
+        new_row = {'model_name': model, 'accuracy' : [df.iloc[0]['overall_accuracy']], 'digits': [digit_counter]}
+        accuracy_df = pd.concat([accuracy_df, pd.DataFrame(new_row)])
+        digit_counter += 1
 
+accuracy_df.to_csv('Results/accuracy/accuracy_scores.csv')
+
+# %%
+# Plot accuracy scores
+
+df = pd.read_csv('Results/accuracy/accuracy_scores.csv', skiprows=1, header=None, names=['idx', 'model_name', 'accuracy', 'digits'] ).drop(columns='idx')
+
+plt.figure(figsize=(8, 5))
+sns.lineplot(data=df, x='digits', y='accuracy', hue='model_name', marker='o')
+plt.xlabel('Digits')
+plt.ylabel('Accuracy %')
+plt.ylim(0, 100)
+plt.grid(True, alpha=0.3)
+plt.legend(title='Model')
+plt.show()
+
+plt.savefig('Results/accuracy/accuracy_plot.png')
 # %%
