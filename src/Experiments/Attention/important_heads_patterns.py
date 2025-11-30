3d2a750df5b6afe8aa12637722a1b9df1f6f70df
julianDesktop <julianramirezcordero@gmail.com>
2025-11-30 04:30:31 +0000

    fixed important_heads

--- src/Experiments/Attention/important_heads_patterns.py
+++ src/Experiments/Attention/important_heads_patterns.py
@@ -16,16 +16,8 @@ model = HookedTransformer.from_pretrained("qwen2.5-3b")
 text = "Is 9432 > 8231? Answer: "
 _, cache = model.run_with_cache(text)
 
-# We want to know what the LAST token (position -1) is looking at
+# pattern for last position
 last_pos = len(model.to_tokens(text)[0]) - 1
 
-# "Who is L24H5 listening to?"
 #%%
 vis = display_attention_heads(model, text, cache, layer=0, position=last_pos)
-
-with open("results/attention_heads_L0.html", "w") as f:
-    f.write(str(vis))
-
-vis
-
-# %%
---
+++ src/Experiments/path_patching/head_to_head.py
@@ -0,0 +1,5 @@
+#%%
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from Interpretability.path_patching import path_patch
