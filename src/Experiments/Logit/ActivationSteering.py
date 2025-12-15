616c080d8be8483893136c3c44746042e3b10e16
AryaBhardia1124 <arya_bhardia@berkeley.edu>
2025-12-15 02:08:56 -0800

    Activation steering code update

--- src/Experiments/Logit/ActivationSteering.py
+++ src/Experiments/Logit/ActivationSteering.py
@@ -24,7 +24,7 @@ yes_id = model.to_single_token(" Yes")
 no_id = model.to_single_token(" No")
 
 Attention_Layer = 24
-Attention_Head = 7
+Attention_Head = 5
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
