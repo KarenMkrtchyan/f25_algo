599babe8c3bd38a4101c8d60c65ca1fa38c52df4
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-12 17:59:32 -0800

    Change model loading from 'qwen2.5-3b' to 'phi-3'

--- src/Experiments/Logit/Token_prediction.py
+++ src/Experiments/Logit/Token_prediction.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 from transformer_lens import utils as Utils
 from utils.model_config import load_model
 
-model = load_model("qwen2.5-3b")
+model = load_model("phi-3")
 
 #example_prompt = "After John and Mary went to the store, John gave a bottle of milk to"
 example_prompt = "Is 9876 > 5432? Answer:"
