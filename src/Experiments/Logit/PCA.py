9a15ca7cc1038a348b782707af1002c60bd20530
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-12 17:30:47 -0800

    Code for Qwen3 PCA

--- src/Experiments/Logit/PCA.py
+++ src/Experiments/Logit/PCA.py
@@ -24,8 +24,8 @@ device = get_device()
 yes_id = model.to_single_token(" Yes")
 no_id = model.to_single_token(" No")
 
-Attention_Layer = 24
-Attention_Head = 9
+Attention_Layer = 13
+Attention_Head = 11
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
