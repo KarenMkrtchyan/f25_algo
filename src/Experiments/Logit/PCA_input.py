c2107247fbd1fae72a1d4983f8dd890f7ed6786f
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-12 17:47:44 -0800

    PCA_input update for Qwen3

--- src/Experiments/Logit/PCA_input.py
+++ src/Experiments/Logit/PCA_input.py
@@ -24,7 +24,7 @@ device = get_device()
 yes_id = model.to_single_token(" Yes")
 no_id = model.to_single_token(" No")
 
-Attention_Layer = 24
+Attention_Layer = 17
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
