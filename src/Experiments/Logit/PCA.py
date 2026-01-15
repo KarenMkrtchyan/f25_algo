bf5e3f448ff24196fb4ab7db6421dfa8497c9bbf
AryaBhardia1124 <arya_bhardia@berkeley.edu>
2026-01-14 23:21:29 -0800

    input update

--- src/Experiments/Logit/PCA.py
+++ src/Experiments/Logit/PCA.py
@@ -27,8 +27,8 @@ device = get_device()
 yes_id = model.to_single_token("Yes")
 no_id = model.to_single_token("No")
 
-Attention_Layer = 28
-Attention_Head = 10
+Attention_Layer = 26
+Attention_Head = 11
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
