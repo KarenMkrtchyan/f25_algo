a0ad4c3362d0bdba8027d383ecc9b6baf70f0536
AryaBhardia1124 <arya_bhardia@berkeley.edu>
2026-01-14 23:41:34 -0800

    more PCA phi

--- src/Experiments/Logit/PCA_input.py
+++ src/Experiments/Logit/PCA_input.py
@@ -27,7 +27,7 @@ device = get_device()
 yes_id = model.to_single_token("Yes")
 no_id = model.to_single_token("No")
 
-Attention_Layer = 21
+Attention_Layer = 10
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
