2552ad2ac21ee733c9172e44feee873ec27fc86f
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-24 16:32:34 -0800

    update model name in file

--- src/Experiments/Logit/activation_patching.py
+++ src/Experiments/Logit/activation_patching.py
@@ -20,7 +20,10 @@ dataset = build_dataset_space(n=100, low=1000, high=9999)
 #model_name = "pythia-70m"
 #model_name = "qwen2.5-3b"
 #model_name = "qwen3-1.7b"
-model_name = "phi-3"
+#model_name = "phi-3"
+model_name = "gemma-2-9b-it"
+#model_name = "llama3-8b-it"
+
 model = load_model(model_name)
 device = get_device()
 
