bc978fdda54db144c88dc75b6f63ed86176e3392
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-25 21:04:33 -0800

    Modify PCA layers configuration

--- src/Experiments/Logit/PCA_input.py
+++ src/Experiments/Logit/PCA_input.py
@@ -31,7 +31,7 @@ no_id = model.to_single_token(" No")
 #no_id = model.to_single_token("No")
 
 #Attention_Layer = 15
-layers = [10, 15, 20]
+layers = [11, 12, 13, 14]
 
 batches_base, batches_src, batches_ans = build_numeric_batches(model, dataset, yes_id, no_id, device)
 num_batches = len(batches_src)
