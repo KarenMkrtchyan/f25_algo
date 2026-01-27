1771f895f6fce81f0bb40e1b22b53472951e8d19
AryaBhardia1124 <arya.bhardia.work@gmail.com>
2026-01-26 19:07:29 -0800

    Update model name to qwen3-4b in activation_patching.py

--- src/Experiments/Logit/activation_patching.py
+++ src/Experiments/Logit/activation_patching.py
@@ -18,13 +18,14 @@ dataset = build_dataset(n=100, low=1000, high=9999)
 #dataset = build_dataset_space(n=100, low=1000, high=9999)
 
 #model_name = "pythia-70m"
-model_name = "qwen2.5-3b"
+#model_name = "qwen2.5-3b"
 #model_name = "qwen3-1.7b"
 #model_name = "phi-3"
 #model_name = "gemma-2-9b-it"
 #model_name = "llama3-8b-it"
 #model_name = "qwen2.5-7b"
 #model_name = "qwen1.5-4b"
+model_name = "qwen3-4b"
 
 model = load_model(model_name, torch_dtype = t.float32)
 device = get_device()
